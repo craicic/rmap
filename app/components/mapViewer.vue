5c6d3187ec1175c91308e7f0d116f0a8248e1bf5
craicic <github.4g46w@passmail.net>
2025-09-09 14:00:00 +0200

    project start

---
+++ app/components/mapViewer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import OpenSeadragon, {Viewer} from 'openseadragon';
+import { ref, onMounted, onUnmounted } from 'vue';
+
+let viewer: Viewer;
+
+onMounted(() => {
+  viewer = OpenSeadragon({
+    id: 'open-seadragon',
+    prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
+    tileSources: '/map.png',
+  });
+});
+
+onUnmounted(() => {
+  if (viewer.isOpen()) {
+    viewer.destroy();
+  }
+});
+</script>
+
+<template>
+  <div id="open-seadragon" style="width: 800px; height: 600px;"></div>
+  <p>Hello !</p>
+</template>
