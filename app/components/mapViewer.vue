9523e1af640d04cf0e944b2eefa769438c2a8867
craicic <github.4g46w@passmail.net>
2025-09-12 17:05:43 +0200

    commit before I switch from nuxt/leaflet to leaflet

--- app/components/mapViewer.vue
+++ app/components/mapViewer.vue
@@ -1,28 +1,48 @@
 <script setup>
+import { latLngBounds, CRS } from 'leaflet';
 import { ref } from 'vue'
 
-const zoom = ref(6);
-const center = ref([47.21322, -1.559482]);
+const tileSize = 256;
+const maxZoom = ref(4);
+const imageSize = tileSize * Math.pow(2, maxZoom.value); // 4096 for 256 * 2^4
+const bounds = ref(latLngBounds([0, 0], [imageSize, imageSize]));
+
+const zoom = ref(1);
+const center = ref(bounds.value.getCenter());
 
 const zoomReset = () => {
-  zoom.value = 6;
-  center.value = [47.21322, -1.559482];
+  zoom.value = 0;
+  center.value = bounds.value.getCenter();
+  console.log(center.value)
+  console.log(CRS.Simple)
 }
 </script>
 
+
 <template>
-  <LMap
-      v-model:zoom="zoom"
-      v-model:center="center"
-      :use-global-leaflet="false"
-      style="height: 350px"
-  >
-    <LTileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-        layer-type="base"
-        name="OpenStreetMap"
-    />
-  </LMap>
+    <LMap
+        v-model:zoom="zoom"
+        v-model:center="center"
+        :bounds="bounds"
+        :max-bounds="bounds"
+        :max-bounds-viscosity="1.0"
+        :min-zoom="0"
+        :max-zoom="maxZoom"
+        :crs="CRS.Simple"
+        :world-copy-jump="false"
+        :use-global-leaflet="false"
+        style="height: 700px"
+    >
+      <LTileLayer
+
+          url="/map/{z}/{x}/{y}.png"
+          :no-wrap="true"
+          :min-zoom="0"
+          :max-zoom="maxZoom"
+          name="treasure-map"
+      />
+    </LMap>
+
+
   <input type="button" value="reset" @click="zoomReset">
 </template>
