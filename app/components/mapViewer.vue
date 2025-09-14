3f2ec72a28a2ccc58224ddf6af316e7042471af6
craicic <github.4g46w@passmail.net>
2025-09-14 11:30:14 +0200

    Now uses leaflet.js without any wrapper. Includes leaflet-rastercoords to fix coordinates problems (use of 'unproject')
    Removes @nuxtjs/leaflet.

--- app/components/mapViewer.vue
+++ app/components/mapViewer.vue
@@ -1,48 +1,71 @@
 <script setup>
-import { latLngBounds, CRS } from 'leaflet';
-import { ref } from 'vue'
+import 'leaflet/dist/leaflet.css';
+import {onMounted, ref} from 'vue'
 
-const tileSize = 256;
-const maxZoom = ref(4);
-const imageSize = tileSize * Math.pow(2, maxZoom.value); // 4096 for 256 * 2^4
-const bounds = ref(latLngBounds([0, 0], [imageSize, imageSize]));
+const tileSize = 256
+const maxZoom = ref(4)
 
-const zoom = ref(1);
-const center = ref(bounds.value.getCenter());
+const imageSize = tileSize * Math.pow(2, maxZoom.value) // e.g., z=2 => 4*256 = 1024
+
+// For CRS.Simple, southWest = [imageHeight, 0], northEast = [0, imageWidth]
+// const simpleBoundsArray = [[imageSize, 0], [0, imageSize]]
+
+const zoom = ref(0)
+const mapId = ref('map')
+let mapInstance;
 
 const zoomReset = () => {
-  zoom.value = 0;
-  center.value = bounds.value.getCenter();
-  console.log(center.value)
-  console.log(CRS.Simple)
+  if (mapInstance) {
+    mapInstance.setView([0, 0], zoom.value, {animate: false})
+  }
 }
+
+onMounted(async () => {
+  // 1) Load Leaflet (ESM namespace)
+  const L = await import('leaflet')
+
+  // 2) Load RasterCoords from the package entry and normalize interop
+  const RCModule = await import('leaflet-rastercoords')
+  // Some bundlers wrap CJS as { default: fn }, and occasionally double-wrap as { default: { default: fn } }
+  let RasterCoords = RCModule?.default ?? RCModule
+  if (RasterCoords?.default) RasterCoords = RasterCoords.default
+
+  // 3) Ensure we actually have the export
+  if (typeof RasterCoords !== 'function') {
+    throw new Error('leaflet-rastercoords module did not export a function/constructor')
+  }
+
+  // 4) Attach to the same L we use
+  L.RasterCoords = RasterCoords
+
+  // 5) Init map
+  mapInstance = L.map(mapId.value, {
+    crs: L.CRS.Simple,
+    center: [0, 0],
+    maxBoundsViscosity: 1.0,
+    worldCopyJump: false,
+  })
+
+  // 6) Support both constructor and factory shapes
+  const isClassLike = RasterCoords.prototype && (RasterCoords.prototype.unproject || RasterCoords.prototype.getMaxBounds)
+  const rc = isClassLike
+      ? new L.RasterCoords(mapInstance, [imageSize, imageSize])
+      : L.RasterCoords(mapInstance, [imageSize, imageSize])
+
+  // 7) Use rc
+  mapInstance.setMaxZoom(maxZoom.value)
+  mapInstance.setView(rc.unproject([imageSize, imageSize]), 2)
+
+  L.tileLayer(`/map/{z}/{x}/{y}.png`, {
+    noWrap: true,
+    maxNativeZoom: (typeof rc.zoomLevel === 'function' ? rc.zoomLevel() : rc.zoomLevel ?? maxZoom.value),
+    bounds: rc.getMaxBounds(),
+  }).addTo(mapInstance)
+})
+
 </script>
 
-
 <template>
-    <LMap
-        v-model:zoom="zoom"
-        v-model:center="center"
-        :bounds="bounds"
-        :max-bounds="bounds"
-        :max-bounds-viscosity="1.0"
-        :min-zoom="0"
-        :max-zoom="maxZoom"
-        :crs="CRS.Simple"
-        :world-copy-jump="false"
-        :use-global-leaflet="false"
-        style="height: 700px"
-    >
-      <LTileLayer
-
-          url="/map/{z}/{x}/{y}.png"
-          :no-wrap="true"
-          :min-zoom="0"
-          :max-zoom="maxZoom"
-          name="treasure-map"
-      />
-    </LMap>
-
-
-  <input type="button" value="reset" @click="zoomReset">
+  <div :id="mapId" style="height: 90vh"></div>
+  <input type="button" value="Reset" @click="zoomReset">
 </template>
