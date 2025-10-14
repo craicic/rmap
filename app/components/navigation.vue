4f09bbfacd386d63f29ca3c0d065e27df4f1c15b
jonathan <jattar02@gmail.com>
2025-10-14 12:29:29 +0200

    refactor: ui import form

--- app/components/navigation.vue
+++ app/components/navigation.vue
@@ -9,11 +9,9 @@ const { user } = useUserSession();
 </script>
 
 <template>
-	<nav class="flex items-center justify-between p-4">
-		<div class="flex items-left gap-2 flex-col">
-			<ULink to="/" class="font-bold p-2 rounded-xl outline outline-primary">RMAP</ULink>
-			<p v-if="user">Bienvenue {{ user.name }}</p>
-		</div>
+	<UHeader title="RMAP" to="/">
+		<p v-if="user">Bienvenue {{ user.name }}</p>
+
 		<ul class="inline-flex gap-4 list-none p-0 m-0">
 			<li>
 				<UButton to="/import">Importer une carte</UButton>
@@ -25,7 +23,7 @@ const { user } = useUserSession();
 				<UButton id="logout" @click="logout">Se déconnecter</UButton>
 			</li>
 		</ul>
-	</nav>
-	<hr />
+	</UHeader>
+
 	<slot />
 </template>
--- app/pages/import.vue
+++ app/pages/import.vue
@@ -1,5 +1,105 @@
-<script setup></script>
+<script setup lang="ts">
+import { nextTick, ref } from 'vue';
+import { navigateTo, useRoute } from '../../.nuxt/imports';
+import type { FormError, FormSubmitEvent } from '@nuxt/ui';
+
+const errorContainer = ref<HTMLElement>();
+
+const loading = ref(false);
+const state = reactive({
+	file: undefined,
+	name: undefined,
+	format: undefined,
+	zoom: 4,
+});
+
+const imgEl = ref<HTMLImageElement | null>(null);
+const format = ref<string | null>(null);
+const name = ref<string | null>(null);
+const formats = ref(['webp', 'png', 'avif']);
+
+const getImageDimensions = (file) => {
+	return new Promise((resolve) => {
+		const img = new Image();
+		img.onload = () => {
+			resolve({
+				width: img.naturalWidth,
+				height: img.naturalHeight,
+			});
+		};
+		img.src = URL.createObjectURL(file);
+	});
+};
+
+const submit = async (...e) => {
+	loading.value = true;
+	const { width, height } = await getImageDimensions(state.file);
+	const form = new FormData();
+	form.append('file', state.file);
+	form.append('name', state.name);
+	form.append('minZoom', '0');
+	form.append('maxZoom', state.zoom.toString());
+	form.append('format', state.format);
+	form.append('width', width.toString());
+	form.append('height', height.toString());
+	try {
+		let response = await $fetch('/api/upload', {
+			method: 'POST',
+			body: form,
+		});
+		navigateTo('/maps/' + response.id);
+	} catch (e) {
+		errorContainer.value!.innerText = `Échec de l\'importation ${e?.message || e}`;
+	}
+	loading.value = false;
+};
+
+const validate = (state: any): FormError[] => {
+	const errors = [];
+	if (!state.file) errors.push({ name: 'file', message: 'Image requise' });
+	return errors;
+};
+</script>
 
 <template>
-	<importForm />
+	<UMain class="grid place-items-center">
+		<UContainer class="flex flex-col items-center gap-4">
+			<h1 class="text-2xl font-bold">Importer une carte</h1>
+			<UForm :state="state" @submit="submit" :validate="validate">
+				<UFormField name="file" class="mb-4">
+					<UFileUpload
+						v-model="state.file"
+						class="w-full min-h-[300px]"
+						accept="image/jpeg, image/png, image/webp, image/avif"
+						label="Déposer votre image ici ou cliquer pour sélectionner"
+					/>
+				</UFormField>
+				<div class="">
+					<UInput
+						required
+						v-model="state.name"
+						type="text"
+						pattern="[a-zA-Z0-9]+"
+						placeholder="Nom"
+						class="mr-4"
+					/>
+					<USelect required v-model="state.format" :items="formats" placeholder="format" />
+					<UFormField :label="`Zoom ${state.zoom}`" class="mt-4">
+						<USlider v-model="state.zoom" :min="0" :max="8" />
+					</UFormField>
+					<USeparator class="mt-6 mb-4" />
+
+					<UButton
+						:loading
+						type="submit"
+						size="xl"
+						:disabled="!state.file || !state.name || !state.format"
+					>
+						<span class="uppercase">Submit</span>
+					</UButton>
+					<p :ref="errorContainer" class="text-red-600"></p>
+				</div>
+			</UForm>
+		</UContainer>
+	</UMain>
 </template>
