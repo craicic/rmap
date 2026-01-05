02253c66ffd794e6021f27894fda5bdcea2a942b
craicic <github.4g46w@passmail.net>
2026-01-05 17:45:07 +0100

    list is now displayed correctly

--- app/pages/maps/list.vue
+++ app/pages/maps/list.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import type {TableColumn} from '@nuxt/ui';
-import {ref} from 'vue';
 import type {TileMapData} from '#shared/info';
 
-
 const route = useRoute()
-const metadata = await $fetch<{ savedMaps: TileMapData[] }>('/api/maps/metadata');
+const data = await $fetch<{ savedMaps: TileMapData[] }>('/api/maps/metadata');
 
-const tableData = computed(() => metadata?.savedMaps || []);
-
-
+const tableData = computed(() => data.savedMaps.slice() || []);
 const columns: TableColumn<TileMapData>[] = [
     {
+        header: 'Name',
         accessorKey: 'originalFile.name',
-        header: 'Name'
     },
     {
+        header: 'Format',
         accessorKey: 'originalFile.format',
-        header: 'Format'
     }
 ];
 </script>
@@ -25,24 +21,20 @@ const columns: TableColumn<TileMapData>[] = [
 <template>
     <UContainer class="flex flex-col items-center gap-4">
         <h1 class="text-4xl font-bold">Liste des cartes</h1>
-        <template v-if="false">
-            <UEmpty
-                icon="i-lucide-file"
-                title="Aucune carte trouvée"
-                description="Il semble qu'aucune carte n'ait été ajoutée. Importez-en une pour commencer"
-                :actions="[{
+        <UEmpty
+            v-if="!tableData.length"
+            icon="i-lucide-file"
+            title="Aucune carte trouvée"
+            description="Il semble qu'aucune carte n'ait été ajoutée. Importez-en une pour commencer"
+            :actions="[{
                         icon: 'i-lucide-plus',
                         label: 'Importer une carte',
                         to: '/import',
                         active: route.path.startsWith('/import')
              }]"/>
-        </template>
 
-        <template>
-            <UTable :data="tableData" :columns="columns" class="flex-1"/>
-        </template>
+        <UTable v-else :data="tableData" :columns="columns" class="flex-1"/>
     </UContainer>
-
 </template>
 
 <style scoped></style>
